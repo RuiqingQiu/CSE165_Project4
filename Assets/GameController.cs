e62b2a6033895d6b6ebc21851d46098e5829649c
Ruiqing Qiu <raymond.qiu1993@gmail.com>
2015-02-17 17:36:15 -0800

    first commit

---
+++ Assets/GameController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+	public GameObject jenga;
+	public GameObject camera;
+	public float rotate_speed = 20.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+		//camera.transform.Rotate(jenga.transform.up * 5.0f * Time.deltaTime);
+		camera.transform.RotateAround (Vector3.zero, jenga.transform.up, rotate_speed * Time.deltaTime);
+
+	}
+}
